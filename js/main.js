6ce8c38fe2bb89935f37c01761c58911c8df620e
FreddMai <fredtein@gmail.com>
2021-11-06 18:10:40 +0200

    Sticky headers and some other css and js fixes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 $(document).ready(function () {
   "use strict";
 
+  // Add or remove active class on main menu.
   $("ul.nav-menu > li").click(function (e) {
     e.preventDefault();
     $("ul.nav-menu > li").removeClass("active");
     $(this).addClass("active");
   });
 
+  // Add or remove active class on sub menu.
+  $("ul.nav-menu-sub > li").click(function (e) {
+    e.preventDefault();
+    $("ul.nav-menu-sub > li").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  // Add or remove hamburger menu on mobile.
   $("div.hamburger").click(function (e) {
     e.preventDefault();
     if ($("div.hamburger").hasClass("active")) {
@@ -17,17 +26,29 @@ $(document).ready(function () {
       $("ul.nav-menu").addClass("active");
     }
   });
+
+  var lastScrollTop = 0,
+    delta = 5;
+
+  var $window = $(window);
+  var windowsize = $window.width();
+  if (windowsize > 950) {
+    $(window).scroll(function (event) {
+      // On scroll.
+      var st = $(this).scrollTop();
+
+      if (Math.abs(lastScrollTop - st) <= delta) return;
+
+      if (st > lastScrollTop) {
+        // If scrolling down.
+        $("nav.navbar").removeClass("sticky");
+        $("nav.navbar-sub").removeClass("sub-sticky");
+      } else {
+        // If scrolling up.
+        $("nav.navbar").addClass("sticky");
+        $("nav.navbar-sub").addClass("sub-sticky");
+      }
+      lastScrollTop = st;
+    });
+  }
 });
-
-// const hamburger = document.getElementsByClassName(".hamburger");
-// const navMenu = document.getElementsByClassName(".nav-menu");
-
-// console.log(hamburger);
-
-// hamburger.addEventListener("click", mobileMenu);
-
-// function mobileMenu() {
-//   console.log("here");
-//   hamburger.classList.toggle("active");
-//   navMenu.classList.toggle("active");
-// }
